4204b0bd2f277cb945dcf7d154bbc31703dc1e84
Sergey <sergey.saevich@helmes.com>
2021-02-02 15:36:05 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,37 +1,98 @@
 /* global OT */
-
-(function closure() {
+var apiKey;
+var sessionId;
+var token;
+(function clo(){
   const video = document.querySelector('#video');
-  if (!video.captureStream) {
-    alert('This browser does not support VideoElement.captureStream(). You must use Google Chrome.');
-    return;
-  }
   const stream = video.captureStream();
-  let publisher;
-  const publish = () => {
-    const videoTracks = stream.getVideoTracks();
-    const audioTracks = stream.getAudioTracks();
-    if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
-      stream.removeEventListener('addtrack', publish);
-      publisher = OT.initPublisher('publisher', {
-        videoSource: videoTracks[0],
-        audioSource: audioTracks[0],
-        fitMode: 'contain',
-        width: 320,
-        height: 240
-      }, (err) => {
-        if (err) {
-          video.pause();
-          alert(err.message);
-        } else {
-          video.play();
-        }
-      });
-      publisher.on('destroyed', () => {
-        video.pause();
-      });
-    }
-  };
   stream.addEventListener('addtrack', publish);
-  publish();
-})();
+  function handleError(error) {
+    if (error) {
+      console.error(error);
+    }
+  }
+  
+  /*   var session = OT.initSession(apiKey, sessionId);
+  
+    // Subscribe to a newly created stream
+    session.on('streamCreated', function streamCreated(event) {
+      var subscriberOptions = {
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+      };
+      session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+    });
+  
+    session.on('sessionDisconnected', function sessionDisconnected(event) {
+      console.log('You were disconnected from the session.', event.reason);
+    }); */
+  
+  
+  
+    let publisher;
+    // initialize the publisher
+    function publish() {
+      debugger
+      const videoTracks = stream.getVideoTracks();
+      const audioTracks = stream.getAudioTracks();
+      if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
+        stream.removeEventListener('addtrack', publish);
+        publisher = OT.initPublisher('publisher', {
+          videoSource: videoTracks[0],
+          audioSource: audioTracks[0],
+          fitMode: 'contain',
+          width: 320,
+          height: 240
+        }, (err) => {
+          if (err) {
+            video.pause();
+            alert(err.message);
+          } else {
+            video.play();
+          }
+        });
+        publisher.on('destroyed', () => {
+          video.pause();
+        });
+        
+      }
+    }
+  
+     
+    //publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+    // Connect to the session
+  /*   session.connect(token, function callback(error) {
+      debugger
+      if (error) {
+        handleError(error);
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        session.publish(publisher, handleError);
+      }
+    }); */
+  
+  
+  // See the config.js file.
+  if (API_KEY && TOKEN && SESSION_ID) {
+    apiKey = API_KEY;
+    sessionId = SESSION_ID;
+    token = TOKEN;
+    initializeSession();
+  } else if (SAMPLE_SERVER_BASE_URL) {
+    // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
+    fetch('https://opentok-web-samples-backend.herokuapp.com' + '/session').then(function fetch(res) {
+      return res.json();
+    }).then(function fetchJson(json) {
+      apiKey = json.apiKey;
+      sessionId = json.sessionId;
+      token = json.token;
+  
+      
+    }).catch(function catchErr(error) {
+      handleError(error);
+      alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+    });
+  }
+  
+})()
