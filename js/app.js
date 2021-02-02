7652f42f192526b76fac126ceb1bf291b36ebcdf
Sergey <sergey.saevich@helmes.com>
2021-02-02 15:58:27 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-/* global OT */
+
 var apiKey;
 var sessionId;
 var token;
-(function clo(){
+(function clojure() {
   const video = document.querySelector('#video');
   const stream = video.captureStream();
   stream.addEventListener('addtrack', publish);
@@ -11,111 +11,68 @@ var token;
       console.error(error);
     }
   }
-  
-  /*   var session = OT.initSession(apiKey, sessionId);
-  
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function streamCreated(event) {
-      var subscriberOptions = {
-        insertMode: 'append',
-        width: '100%',
-        height: '100%'
-      };
-      session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
-    });
-  
-    session.on('sessionDisconnected', function sessionDisconnected(event) {
-      console.log('You were disconnected from the session.', event.reason);
-    }); */
-  
-  
-  
-    let publisher;
-    // initialize the publisher
-    function publish() {
-      debugger
-      const videoTracks = stream.getVideoTracks();
-      const audioTracks = stream.getAudioTracks();
-      if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
-        stream.removeEventListener('addtrack', publish);
-        publisher = OT.initPublisher('publisher', {
-          videoSource: videoTracks[0],
-          audioSource: audioTracks[0],
-          fitMode: 'contain',
-          width: 320,
-          height: 240
-        }, (err) => {
-          if (err) {
-            video.pause();
-            alert(err.message);
-          } else {
-            video.play();
-          }
-        });
-        publisher.on('destroyed', () => {
+
+  let publisher;
+  // initialize the publisher
+  function publish() {
+    const videoTracks = stream.getVideoTracks();
+    const audioTracks = stream.getAudioTracks();
+    if (!publisher && videoTracks.length > 0 && audioTracks.length > 0) {
+      stream.removeEventListener('addtrack', publish);
+      publisher = OT.initPublisher('publisher', {
+        videoSource: videoTracks[0],
+        audioSource: audioTracks[0],
+        fitMode: 'contain',
+      }, (err) => {
+        if (err) {
           video.pause();
-        });
+          alert(err.message);
+        } else {
+          video.play();
+        }
+      });
+      publisher.on('destroyed', () => {
+        video.pause();
+      });
 
+      var session = OT.initSession(apiKey, sessionId);
 
-        var session = OT.initSession(apiKey, sessionId);
-  
-        // Subscribe to a newly created stream
-        session.on('streamCreated', function streamCreated(event) {
-          var subscriberOptions = {
-            insertMode: 'append',
-            width: '100%',
-            height: '100%'
-          };
-          session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
-        });
-      
-        session.on('sessionDisconnected', function sessionDisconnected(event) {
-          console.log('You were disconnected from the session.', event.reason);
-        })
+      // Subscribe to a newly created stream
+      session.on('streamCreated', function streamCreated(event) {
+        var subscriberOptions = {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%'
+        };
+        session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+      });
 
+      session.on('sessionDisconnected', function sessionDisconnected(event) {
+        console.log('You were disconnected from the session.', event.reason);
+      })
 
+      session.connect(token, function callback(error) {
+        debugger
+        if (error) {
+          handleError(error);
+        } else {
+          // If the connection is successful, publish the publisher to the session
+          session.publish(publisher, handleError);
+        }
+      })
 
-        session.connect(token, function callback(error) {
-          debugger
-          if (error) {
-            handleError(error);
-          } else {
-            // If the connection is successful, publish the publisher to the session
-            session.publish(publisher, handleError);
-          }
-        })
-        
-      }
     }
-  
-     
-    //publisher = OT.initPublisher('publisher', publisherOptions, handleError);
-    // Connect to the session
-  /*   session.connect(token, function callback(error) {
-      debugger
-      if (error) {
-        handleError(error);
-      } else {
-        // If the connection is successful, publish the publisher to the session
-        session.publish(publisher, handleError);
-      }
-    }); */
-  
-  
+  }
 
-    // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
-    fetch('https://opentok-web-samples-backend.herokuapp.com' + '/session').then(function fetch(res) {
-      return res.json();
-    }).then(function fetchJson(json) {
-      apiKey = json.apiKey;
-      sessionId = json.sessionId;
-      token = json.token;
-  
-      
-    }).catch(function catchErr(error) {
-      handleError(error);
-      alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
-    });
-  
-  
+  // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
+  fetch('https://opentok-web-samples-backend.herokuapp.com' + '/session').then(function fetch(res) {
+    return res.json();
+  }).then(function fetchJson(json) {
+    apiKey = json.apiKey;
+    sessionId = json.sessionId;
+    token = json.token;
+  }).catch(function catchErr(error) {
+    handleError(error);
+    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
+  });
 })()
